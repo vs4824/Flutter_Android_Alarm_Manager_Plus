a97744e5c4ac7db865db5a34178f14cbaef3cfec
Vaibhav Sharma <vaibhav.2022@tecorb.co>
2023-05-11 16:27:31 +0530

    Flutter Android Alarm Manager Plus

---
+++ android/app/src/main/kotlin/com/example/flutter_android_alarm_manager_plus/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_android_alarm_manager_plus
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'dart:developer' as developer;
+import 'dart:isolate';
+import 'dart:math';
+import 'dart:ui';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter/material.dart';
+
+/// The [SharedPreferences] key to access the alarm fire count.
+const String countKey = 'count';
+
+/// The name associated with the UI isolate's [SendPort].
+const String isolateName = 'isolate';
+
+/// A port used to communicate from a background isolate to the UI isolate.
+ReceivePort port = ReceivePort();
+
+/// Global [SharedPreferences] object.
+SharedPreferences? prefs;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Register the UI isolate's SendPort to allow for communication from the
+  // background isolate.
+  IsolateNameServer.registerPortWithName(
+    port.sendPort,
+    isolateName,
+  );
+  prefs = await SharedPreferences.getInstance();
+  if (!prefs!.containsKey(countKey)) {
+    await prefs!.setInt(countKey, 0);
+  }
+
+  runApp(const AlarmManagerExampleApp());
+}
+
+/// Example app for Espresso plugin.
+class AlarmManagerExampleApp extends StatelessWidget {
+  const AlarmManagerExampleApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: const Color(0x9f4376f8),
+      ),
+      home: const _AlarmHomePage(),
+    );
+  }
+}
+
+class _AlarmHomePage extends StatefulWidget {
+  const _AlarmHomePage({Key? key}) : super(key: key);
+
+  @override
+  _AlarmHomePageState createState() => _AlarmHomePageState();
+}
+
+class _AlarmHomePageState extends State<_AlarmHomePage> {
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    AndroidAlarmManager.initialize();
+
+    // Register for events from the background isolate. These messages will
+    // always coincide with an alarm firing.
+    port.listen((_) async => await _incrementCounter());
+  }
+
+  Future<void> _incrementCounter() async {
+    developer.log('Increment counter!');
+    // Ensure we've loaded the updated count from the background isolate.
+    await prefs?.reload();
+
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // The background
+  static SendPort? uiSendPort;
+
+  // The callback for our alarm
+  @pragma('vm:entry-point')
+  static Future<void> callback() async {
+    developer.log('Alarm fired!');
+    // Get the previous cached count and increment it.
+    final prefs = await SharedPreferences.getInstance();
+    final currentCount = prefs.getInt(countKey) ?? 0;
+    await prefs.setInt(countKey, currentCount + 1);
+
+    // This will be null if we're running in the background.
+    uiSendPort ??= IsolateNameServer.lookupPortByName(isolateName);
+    uiSendPort?.send(null);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyle = Theme.of(context).textTheme.headlineMedium;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Android alarm manager plus example'),
+        elevation: 4,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Alarm fired $_counter times',
+              style: textStyle,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Total alarms fired: ',
+                  style: textStyle,
+                ),
+                Text(
+                  prefs?.getInt(countKey).toString() ?? '',
+                  key: const ValueKey('BackgroundCountText'),
+                  style: textStyle,
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              key: const ValueKey('RegisterOneShotAlarm'),
+              onPressed: () async {
+                await AndroidAlarmManager.oneShot(
+                  const Duration(seconds: 5),
+                  // Ensure we have a unique alarm ID.
+                  Random().nextInt(pow(2, 31) as int),
+                  callback,
+                  exact: true,
+                  wakeup: true,
+                );
+              },
+              child: const Text(
+                'Schedule OneShot Alarm',
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
